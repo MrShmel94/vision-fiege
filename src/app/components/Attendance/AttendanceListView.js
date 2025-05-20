6810edda25064d506dd351ec7902f60ea923d648
vision <mrshmel94@gmail.com>
2025-05-20 23:44:09 +0200

    change url

--- src/app/components/Attendance/AttendanceListView.js
+++ src/app/components/Attendance/AttendanceListView.js
@@ -46,6 +46,7 @@ import ClearAllIcon from '@mui/icons-material/ClearAll';
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
 
+
 const STATUSES = {};
 const SHIFTS = {};
 const allDepartments = new Set(["Site"]);
@@ -293,7 +294,7 @@ export default function AttendanceView() {
     fetchConfig();
     stompClient.current = new Client({
       webSocketFactory: () =>
-        new SockJS(BASE_URL, null, { xhrWithCredentials: true }),
+        new SockJS(`${BASE_URL}/attendance`, null, { xhrWithCredentials: true }),
       reconnectDelay: 5000,
       onConnect: (frame) => {
         const {startDate, endDate} = getDateRange(selectedMonth);
